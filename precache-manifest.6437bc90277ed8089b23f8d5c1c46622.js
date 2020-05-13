027055b6743f41c8754dcc42cc4b8a154e1c075a
yasinkalkan <yasin.kalkan@medyasoft.com.tr>
2020-05-13 13:17:24 +0300

    Updates

---
+++ precache-manifest.6437bc90277ed8089b23f8d5c1c46622.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0c6fd61acb9649fa16cbd126b2c053",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3303726cb4d953fe0ec9",
+    "url": "/portfolio/static/css/2.a5b678f8.chunk.css"
+  },
+  {
+    "revision": "5d25f2f079816c344876",
+    "url": "/portfolio/static/css/main.e1fe786f.chunk.css"
+  },
+  {
+    "revision": "3303726cb4d953fe0ec9",
+    "url": "/portfolio/static/js/2.5203e53e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.5203e53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d25f2f079816c344876",
+    "url": "/portfolio/static/js/main.9a015933.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "0c50e82b60d542fafb5a299c10bc843f",
+    "url": "/portfolio/static/media/animation.0c50e82b.gif"
+  },
+  {
+    "revision": "34be046f70411fd3d65f9b2ca9979d26",
+    "url": "/portfolio/static/media/icomoon.34be046f.eot"
+  },
+  {
+    "revision": "488c69066d935405fb5301a3a4c057bd",
+    "url": "/portfolio/static/media/icomoon.488c6906.woff"
+  },
+  {
+    "revision": "515b1163c65ab418ef6da125cec38812",
+    "url": "/portfolio/static/media/icomoon.515b1163.svg"
+  },
+  {
+    "revision": "9f3e4b2dd1a85a209c407e009a3a320c",
+    "url": "/portfolio/static/media/icomoon.9f3e4b2d.ttf"
+  },
+  {
+    "revision": "52863eb01a91128608baff016bff8c17",
+    "url": "/portfolio/static/media/me.52863eb0.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
