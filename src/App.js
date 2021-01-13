a5e67b1f98310d0b77367e2b7cac3831315bce95
yasgo <yasinburakkalkan@gmail.com>
2021-01-13 19:58:13 +0300

    Router development.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
-import Header from './components/layout/header';
-import Footer from './components/layout/footer';
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
-import Home from './pages/home'
-import Me from './pages/me'
-import Portfolio from './pages/portfolio'
-import PortfolioDetail from './pages/portfolio-detail'
-import ErrorPage from './pages/404'
+import Home from "./pages/home";
+import Me from "./pages/me";
+import Portfolio from "./pages/portfolio";
+import PortfolioDetail from "./pages/portfolio-detail";
+import ErrorPage from "./pages/404";
 
 function App() {
-    const DATA_URL = 'https://raw.githubusercontent.com/yasinkalkan/yasinkalkan.github.io/master/data.json';
+    const DATA_URL = "https://raw.githubusercontent.com/yasinkalkan/yasinkalkan.github.io/master/data.json";
     const dispatch = useDispatch();
 
     useEffect(() => {
         axios.get(DATA_URL)
             .then(function (response) {
                 dispatch({
-                    type: 'DATA_CHANGE',
+                    type: "DATA_CHANGE",
                     data: response.data
                 })
             })
@@ -30,18 +30,17 @@ function App() {
     }, [DATA_URL, dispatch])
 
     return (
-        <Router>
+        <HashRouter>
             <Header />
             <Footer />
             <Switch>
-
                 <Route exact path="/" component={Home} />
                 <Route exact path="/me" component={Me} />
                 <Route exact path="/portfolio" component={Portfolio} />
                 <Route exact path="/portfolio-detail/:id" component={PortfolioDetail} />
                 <Route component={ErrorPage} />
             </Switch>
-        </Router>
+        </HashRouter>
     );
 }
 
